20b7ca5c40fa15be4d4563c533b70a6b119c4aea
Kelvin Mitau Kyendwa <kelvinkyendwa@gmail.com>
2022-08-25 10:38:19 +0300

    Update Sql.js

--- models/Sql.js
+++ models/Sql.js
@@ -2,9 +2,9 @@ const mysql = require("mysql");
 const connection = mysql.createConnection({
 	host: "127.0.0.1",
 	port: 3307,
-	user: "wm_api_user",
-	password: "wm_ser_1234",
-	database: "wm_api",
+	user: "",
+	password: "",
+	database: "",
 });
 
 connection.connect(function (err) {
